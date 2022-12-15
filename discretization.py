f0abcc7dd281484267caeff1410a7b51d99e0814
jsanders1999 <julian.arthur.sanders@gmail.com>
2022-12-15 11:23:46 +0100

    Added stopping criteria to Jac and GS methods

--- discretization.py
+++ discretization.py
@@ -16,77 +16,37 @@ def A(N, eps):
 
     Returns
     -------
-    sparse array of floats (format : CSC, dim: (N-2)*(N-2))
+    sparse array of floats (format : CSC, dim: (N-1)*(N-1))
         The discretization matrix
     """
-
     h = 1/N
     L = 1/h**2*( 2*sp.sparse.eye(N-1, format='csc') - sp.sparse.eye(N-1, k=1, format='csc') - sp.sparse.eye(N-1, k=-1, format='csc') )
     D = 1/h*( sp.sparse.eye(N-1, format='csc') - sp.sparse.eye(N-1, k=-1, format='csc') )
     return eps*L + D
 
-def A_Full(N,eps):
+def A_Full(N, eps):
+    """
+    A function that resturns the upper, diagonal and lower components of the
+    discretization matrix for a system with N nodes and parameter epsilon.
+
+    Parameters
+    ----------
+    N : int
+        The number of grid points 
+    eps : float64
+        The epsilon parameter from the differential equation
+
+    Returns
+    -------
+    4 arrays of floats (dim: (N-1)*(N-1))
+        The discretization matrix components
+    """
     h   = 1/N
     D   = np.diag(np.ones(N-1)*((2*eps)/(h**2)) + 1/h,0)
     E   = np.diag(np.ones(N-2)*((-eps/(h**2) - 1/h)), -1)
     F   = np.diag(np.ones(N-2)* (-eps/(h**2)), 1)
     A   = D + E + F
     return A, D, E, F
-
-def Jacobi(N,eps):
-    A_Full, D, E, F = A(N,eps)
-    B_Jac           = (-E) + (-F)
-    return B_Jac
-
-def Jacobi_Iteration(N,eps):
-    b       = f(N,eps, BC = [1,0])
-    #print("f = ", b, "\n")
-    A,D,E,F = A_Full(N,eps)
-    #print("A = ", A, "\n")
-    #print("D = ", D, "\n")
-    #print("E = ", E, "\n")
-    #print("F = ", F, "\n")
-    u       = np.zeros(N-1)
-    #print("u = ", u, "\n")
-    z       = np.zeros(N-1)
-    #print("z = ", z, "\n")
-    j       = 0
-    while j<30:
-        for i in range(N-1):
-            z[i] = (b[i] - A[i,:i]@u[:i] - A[i,i+1:]@u[i+1:])/A[i,i]
-            #print("z = \n", z, "\n")
-            #print("b[i] = \n", b[i], "\n")
-            #print("A[i, 0:(i-1)] = \n", A[i, 0:(i-1)], "\n")
-            #print("u[0:(i-1)] = \n", u[0:(i-1)], "\n")
-            #print("A[0,i:(N-1)] = \n", A[0,i:(N-1)], "\n")
-            #print("u[(i):(N-1)] = \n", u[(i):(N-1)], "\n")
-            #print("A[i,i] = \n", A[i,i], "\n")
-        j = j+1
-        u = z
-    return A, b, z, u
-
-def Gauss_Seidel(N,eps,Form):
-    if form == "Backward":
-        A_Full, D, E, F = A(N,eps)
-        B_GS = np.linalg.inv(np.identity(N))
-    if form == "Forward":
-        A_Full, D, E, F = A(N,eps)
-        B_GS = np.linalg.inv(np.identity(N))
-    else:                                       #Symmetric
-        B_GS = 1
-    return B_GS
-
-def Gauss_Seidel_Iteration(N,eps):
-    b       = f(N,eps, BC = [1,0])
-    A,D,E,F = A_Full(N,eps)
-    u       = np.zeros(N-1)
-    j = 0
-    while j < 15:
-        for i in range(N-1):
-            u[i] = (b[i] - A[i,:i]@u[:i] - A[i,i+1:]@u[i+1:])/A[i,i]
-        j = j+1
-    
-    return A,b,u
 
 def f(N, eps, BC = [0,0]):
     """
@@ -109,6 +69,120 @@ def f(N, eps, BC = [0,0]):
     res[0]  +=  BC[0]*(eps/h**2+1/h)
     res[-1] +=  BC[1]*(eps/h**2)
     return res
+
+def B_Jacobi(N, eps):
+    """
+    A function that resturns the Jaboci residual iteration matrix given N and eps.
+
+    Parameters
+    ----------
+    N : int
+        The number of grid points 
+    eps : float64
+        The epsilon parameter from the differential equation
+
+    Returns
+    -------
+    array of floats (dim: (N-1)*(N-1))
+        The residual iteration matrix of the Jabobi method
+    """
+    A_Full, D, E, F = A_Full(N,eps)
+    B_Jac           = (-E) + (-F)
+    return B_Jac
+
+def B_Gauss_Seidel(N, eps, form):
+    """
+    A function that resturns the Gauss Seidel residual iteration matrix given N and eps.
+
+    Parameters
+    ----------
+    N : int
+        The number of grid points 
+    eps : float64
+        The epsilon parameter from the differential equation
+    form : string, "Forward" or "Backward"
+        Whether to implement "Forward" or "Backward" Gauss Seidel. 
+
+    Returns
+    -------
+    array of floats (dim: (N-1)*(N-1))
+        The residual iteration matrix of the GS method
+    """
+    if form == "Backward":
+        A_Full, D, E, F = A_Full(N,eps)
+        B_GS = np.linalg.inv(np.identity(N)) #WIP
+    if form == "Forward":
+        A_Full, D, E, F = A_Full(N,eps)
+        B_GS = np.linalg.inv(np.identity(N)) #WIP
+    else:                                       #Symmetric
+        B_GS = 1
+    return B_GS
+
+def Jacobi_Iteration(N, eps, tol):
+    """
+    A function that solves Ax=f using a Jabobi iteration method. A is computed using N and eps.
+    tol determines the stopping criteria
+
+    Parameters
+    ----------
+    N : int
+        The number of grid points 
+    eps : float64
+        The epsilon parameter from the differential equation
+    tol : float64
+        The stopping criterion for the residual. 
+
+    Returns
+    -------
+
+    """
+    MAX_IT  = 30
+    b       = f(N,eps, BC = [1,0])
+    A,D,E,F = A_Full(N,eps)
+    u       = np.zeros(N-1)
+    z       = np.zeros(N-1)
+    B       = B_Jacobi(N, eps)
+    r       = b                     #Starting residual for u = np.zeros(N-1)
+    for j in range(MAX_IT):
+        for i in range(N-1):
+            z[i] = (b[i] - A[i,:i]@u[:i] - A[i,i+1:]@u[i+1:])/A[i,i]
+        u = z
+        r = B@r
+        if np.linalg.norm(r)/np.linalg.norm(b) <= tol:
+            return u, r, j
+    return u, r, MAX_IT
+
+def Gauss_Seidel_Iteration_Forward(N, eps, tol):
+    """
+    A function that solves Ax=f using a Forward Gauss Seidel method. A is computed using N and eps.
+    tol determines the stopping criteria.
+
+    Parameters
+    ----------
+    N : int
+        The number of grid points 
+    eps : float64
+        The epsilon parameter from the differential equation
+    tol : float64
+        The stopping criterion for the residual. 
+
+    Returns
+    -------
+
+    """
+    MAX_IT  = 30
+    b       = f(N,eps, BC = [1,0])
+    A,D,E,F = A_Full(N,eps)
+    u       = np.zeros(N-1)
+    B       = B_GS(N, eps, "Forward")
+    r       = b                     #Starting residual for u = np.zeros(N-1)
+    for j in range(MAX_IT):
+        for i in range(N-1):
+            u[i] = (b[i] - A[i,:i]@u[:i] - A[i,i+1:]@u[i+1:])/A[i,i]
+        r = B@r
+        if np.linalg.norm(r)/np.linalg.norm(b) <= tol:
+            return u, r, j
+    return u, r, MAX_IT
 
 def simpleSolve(N, eps, BC = [0,0]):
     """
@@ -154,7 +228,7 @@ def refSol(N,eps):
     """
     x   = np.linspace(0,1,N+1)
     y   = (np.exp(x/eps) - np.exp(1/eps))/(1-np.exp(1/eps))
-    return x,y
+    return x, y
 
 
 #def u(x,eps):
